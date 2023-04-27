3e61d7866a8c4b16ce91fcf62982219520bcc281
Rati Chiewlian <rchiewli@z3t8c3.42bangkok.com>
2023-04-27 22:14:09 +0700

    add render3-2

--- src/process/key_hook.c
+++ src/process/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: rchiewli <rchiewli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 22:26:45 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/04/27 20:27:03 by rchiewli         ###   ########.fr       */
+/*   Updated: 2023/04/27 22:13:56 by rchiewli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,19 +53,15 @@ void	player_move(t_vei pos, char **map, int mode)
 
 int	hook(int key, t_pro *p)
 {
-	t_vei	player;
 	float		step = 0.15;
 
-	printf("key -> %d\n", key);
 	if (key == 53)
 	{
 		mlx_clear_window(p->mlx.mlx, p->mlx.win);
 		exit(0);
 	}
-	player = get_pos_spt(p->map.map, 1);
 	if (key == 13)
 	{
-		// player_move(player, p->map.map, 0);
 		if(p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + p->di.x * step)] != '1')
 			p->tline.pos.x += p->di.x * step;
 		if(p->map.map[(int)(p->tline.pos.y + p->di.y * step)][(int)(p->tline.pos.x)] != '1')
@@ -73,22 +69,6 @@ int	hook(int key, t_pro *p)
 	}
 	else if (key == 0)
 	{
-		// float right_x = p->di.y;
-		// float right_y = -p->di.x;
-
-		// // Check if the next position to the right in the x direction is '0'
-		// if (p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + right_x * step)] != '1')
-		// 	p->tline.pos.x += right_x * step;
-		// // Check if the next position to the right in the y direction is '0'
-		// if (p->map.map[(int)(p->tline.pos.y + right_y * step)][(int)(p->tline.pos.x)] != '1')
-		// 	p->tline.pos.y += right_y * step;
-
-		// player_move(player, p->map.map, 1);
-		// if(p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + (-1 * p->di.y) * step)] != '1')
-		// 	p->tline.pos.x += (p->di.y * -1) * step;
-		// if(p->map.map[(int)(p->tline.pos.y + p->di.x * step)][(int)(p->tline.pos.x)] != '1')
-		// 	p->tline.pos.y += p->di.x * step;
-		printf("wall left = %c\n", p->map.map[(int)(p->tline.pos.y - p->di.x * step)][(int)(p->tline.pos.x + p->di.y * step)]);
 		if (p->map.map[(int)(p->tline.pos.y - p->di.x * step)][(int)(p->tline.pos.x + p->di.y * step)] != '1')
 		{
 			p->tline.pos.x += p->di.y * step;
@@ -97,22 +77,6 @@ int	hook(int key, t_pro *p)
 	}
 	else if (key == 2)
 	{
-		player_move(player, p->map.map, 2);
-
-		// Calculate the left direction vector
-		// float left_x = -p->di.y;
-		// float left_y = p->di.x;
-
-		// // Check if the next position to the left in the x direction is '0'
-		// if (p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + left_x * step)] != '1')
-		// 	p->tline.pos.x += left_x * step;
-		// // Check if the next position to the left in the y direction is '0'
-		// if (p->map.map[(int)(p->tline.pos.y + left_y * step)][(int)(p->tline.pos.x)] != '1')
-		// 	p->tline.pos.y += left_y * step;
-		// // if(p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + (-1 * p->di.y) * step)] == '0')
-		// // 	p->tline.pos.x += (p->di.y * -1) * step;
-		// // if(p->map.map[(int)(p->tline.pos.y + p->di.x * step)][(int)(p->tline.pos.x)] == '0')
-		// // 	p->tline.pos.y += p->di.x * step;
 		if (p->map.map[(int)(p->tline.pos.y + p->di.x * step)][(int)(p->tline.pos.x - p->di.y * step)] != '1')
 		{
 			p->tline.pos.x += -p->di.y * step;
@@ -121,7 +85,6 @@ int	hook(int key, t_pro *p)
 	}
 	else if (key == 1)
 	{
-		player_move(player, p->map.map, 3);
 		if(p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x - p->di.x * step)] != '1')
 			p->tline.pos.x -= p->di.x * step;
 		if(p->map.map[(int)(p->tline.pos.y - p->di.y * step)][(int)(p->tline.pos.x)] != '1')
@@ -131,10 +94,7 @@ int	hook(int key, t_pro *p)
 		rotate_hook(-5.0, p);
 	else if (key == 124)
 		rotate_hook(5.0, p);
-	// mlx_destroy_image(p->mlx.mlx, p->mlx.win);
-	printf("c = %c and %c\n", p->map.map[(int)(p->tline.pos.y)][(int)(p->tline.pos.x + p->di.x * step)], p->map.map[(int)(p->tline.pos.y + p->di.y * step)][(int)(p->tline.pos.x)]);
-	printf("p->tline.pos.y -> %d\n", (int)p->tline.pos.y);
-	printf("p->tline.pos.x -> %d\n", (int)p->tline.pos.x);
+	player_move(p->map.map, 1);
 	return (0);
 }
 
